bfa8bcc92abcd0b9cd489002003b9bdb9391007a
Amarnath8124 <amarnathamar8124@gmail.com>
2024-07-31 21:50:56 +0530

    modified

--- src/org/git/GitHub.java
+++ src/org/git/GitHub.java
@@ -1,20 +1,18 @@
 package org.git;
 
 public class GitHub {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hello Git");
 	}
-<<<<<<< HEAD
-    
-	
+
+	private void payment() {
+		System.out.println("payment");
+	}
+
 	private void addToCart() {
 		System.out.println("Add to cart");
-=======
-    private void payment() {
-		System.out.println("payment");
->>>>>>> 937f13e42b583e57ff73094fd052e83cb74d0d87
 
 	}
 }
